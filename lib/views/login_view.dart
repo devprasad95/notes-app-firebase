85f083fd1d802e20778ca9f4ce8ae82c52735f4b
Dev Prasad <99073413+devprasad95@users.noreply.github.com>
2022-07-28 15:49:28 +0530

    step-8

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -63,8 +63,17 @@ class _LoginViewState extends State<LoginView> {
                   email: email,
                   password: password,
                 );
-                Navigator.of(context)
-                    .pushNamedAndRemoveUntil(notesRoute, (route) => false);
+                final user = FirebaseAuth.instance.currentUser;
+                if (user?.emailVerified ?? false) {
+                  //user email is verified
+                  Navigator.of(context)
+                      .pushNamedAndRemoveUntil(notesRoute, (route) => false);
+                }
+                {
+                  //user's email is not verified
+                  Navigator.of(context).pushNamedAndRemoveUntil(
+                      verifyEmailRoute, (route) => false);
+                }
               } on FirebaseAuthException catch (e) {
                 if (e.code == 'user-not-found') {
                   await showErrorDialog(context, 'User not found');
